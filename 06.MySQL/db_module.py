6734a1b31416761a2e039b2c2e9e964ad6ae541e
Cheolkie Kim <ckiekim@naver.com>
2021-08-26 17:28:20 +0900

    MySQL 실습 프로젝트

--- 06.MySQL/db_module.py
+++ 06.MySQL/db_module.py
@@ -1,5 +1,6 @@
 import pymysql
 
+# 1. 월별 매출/이익
 def get_monthly_data(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
@@ -18,14 +19,74 @@ def get_monthly_data(config):
     conn.close()
     return results
 
-def set_month(config, params):
+# 2. 거래처별 매출/이익
+def get_data_by_company(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
 
     sql = """
-        update sales set sid=%s, sunit=%s where sid=%s;
+        SELECT scompany, SUM(revenue) AS revenue, SUM(profit) AS profit
+            FROM sales_book
+            GROUP BY scompany
+            ORDER BY revenue desc;
     """
-    cur.execute(sql, params)
+    cur.execute(sql)
+    results = cur.fetchall()
 
     cur.close()
     conn.close()
+    return results
+
+# 3. 거래처별 판매상품 및 수량
+def get_products_by_company(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+
+    sql = """
+        SELECT scompany, pname, SUM(sunit) AS unit
+            FROM sales_book
+            GROUP BY scompany, pname
+            ORDER BY scompany;
+    """
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return results
+
+# 4. 상품별 판매수량/매출/이익
+def get_data_by_products(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+
+    sql = """
+        SELECT pname, SUM(sunit) AS unit,
+		        SUM(revenue) AS revenue, SUM(profit) AS profit
+            FROM sales_book
+            GROUP BY pid
+            ORDER BY pid;
+    """
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return results
+
+# 5. 카테고리별 매출/이익
+def get_data_by_category(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+
+    sql = """
+        SELECT pcategory, SUM(revenue) AS revenue, SUM(profit) AS profit
+            FROM sales_book
+            GROUP BY pcategory;
+    """
+    cur.execute(sql)
+    results = cur.fetchall()
+
+    cur.close()
+    conn.close()
+    return results
